096ffdb8ef72a3134f9fc96eb8c670eb7cdbfc35
yannick <yannick.dominguez@gmail.com>
2014-06-09 21:57:42 +0200

    added info and error printing as each command is played

--- hxsubmit/Git.hx
+++ hxsubmit/Git.hx
@@ -8,9 +8,15 @@ class Git
     public static function push(remote):Array<Command>
     {
         return [
-            ['push', remote, Constants.DEFAULT_BRANCH],
-            ['push', remote, Constants.GIT_TAGS]
-        ].map(getGitCommand.bind());
+            getGitCommand(
+                    ['push', remote, Constants.DEFAULT_BRANCH],
+                    'could not push to remote: ' + remote + ' ,on branch: ' + Constants.DEFAULT_BRANCH,
+                    'pushing changes to remote: ' + remote + ' ,on branch: ' + Constants.DEFAULT_BRANCH),
+            getGitCommand(
+                    ['push', remote, Constants.GIT_TAGS],
+                    'could not push tags to remote: ' + remote + ' ,on branch: ' + Constants.DEFAULT_BRANCH,
+                    'pushing tags to remote: ' + remote + ' ,on branch: ' + Constants.DEFAULT_BRANCH)
+        ];
     }
 
     public static function commit(version, comment):Array<Command>
@@ -19,17 +25,28 @@ class Git
             comment = 'update to version: ' + version;
 
         return [
-            ['add', Constants.HAXELIB_JSON],
-            ['commit', '-m', comment],
-            ['tag', version]
-         ].map(getGitCommand.bind());
+            getGitCommand(
+                    ['add', Constants.HAXELIB_JSON], 
+                    'could not stage haxelib.json',
+                    'staging haxelib.json'),
+            getGitCommand(
+                    ['commit', '-m', comment],
+                    'could not commit',
+                    'commit with message: ' + comment),
+            getGitCommand(
+                    ['tag', version],
+                    'could not create a new tag', 
+                    'creating tag: ' + version)
+        ];
     }
 
-    static function getGitCommand(args):Command
+    static function getGitCommand(args, err, info):Command
     {
         return {
             bin: 'git',
-            args: args
+            args: args,
+            err: err,
+            info: info
         }
     }
 }   
