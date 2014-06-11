51ed8839f44e919efeeab24090b8fa88123fa200
yannick <yannick.dominguez@gmail.com>
2014-06-11 22:05:51 +0200

    reorganizing test files

---
+++ test/TestResources.hx
@@ -0,0 +1,39 @@
+package;
+
+import buddy.*;
+using buddy.Should;
+
+class TestResources extends BuddySuite implements Buddy {
+    public function new() {
+        describe('resource', function () {
+
+            describe('#licence', function () {
+                it('should generate an MIT licence', function () {
+                    var licence = Licence.createMIT(
+                        Date.now().getFullYear(),
+                        'test holder');
+                    licence.indexOf(Std.string(Date.now().getFullYear())).should.not.be(-1);
+                    licence.indexOf('test holder').should.not.be(-1);
+                });
+            });
+            
+            describe('#travis', function () {
+                it('should generate a travis file');
+            });
+
+            describe('#main', function () {
+                it('should generate a main Haxe file');
+            });
+
+            describe('#hxml', function () {
+                it('should generate an .hxml file');
+            });
+
+            describe('#readme', function () {
+                it('should generate a readme file');
+            });
+        });
+    }
+
+}
+
