54645d8fae7f654f099ee071703f36000afb2e4d
yannick <yannick.dominguez@gmail.com>
2014-06-12 23:13:25 +0200

    added project init functionalities

--- majhaxe/commands/Init.hx
+++ majhaxe/commands/Init.hx
@@ -1,5 +1,4 @@
-package commands;
-
+package commands; 
 import Command;
 
 /**
@@ -12,6 +11,32 @@ class Init
         var input = UserInput.init(io.output, io.input);
 
         var commands = [Git.init()];
+
+        commands.push({
+            info: 'creating a licence file',
+            err: 'could not create a licence file',
+            cmd: func(function () {
+                io.write('licence.md', Resources.createMIT(Date.now().getFullYear(), input.holder));
+            })
+        });
+
+        commands.push({
+            info: 'creating a travis file',
+            err: 'could not create a travis file',
+            cmd: func(function () {
+                io.write('.travis.yml', Resources.createTravis(input.dependencies, 'haxe build.hxml'));
+            })
+        });
+
+        commands.push({
+            info: 'creating a main haxe file',
+            err: 'could not create a main haxe file',
+            cmd: func(function () {
+                io.write('.travis.yml', Resources.createMain(input.project));
+            })
+        });
+
+
         return commands;
     }
 }
